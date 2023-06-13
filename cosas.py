73c1ab64acb582c0556ebdd289d1ef7d74872f74
Cristian Navarro <cristiannavarro278@aragon.unam.mx>
2023-06-13 16:10:36 -0600

    se agrega el proyecto

---
+++ cosas.py
@@ -0,0 +1,7 @@
+class Alumno:
+    facultad = "FES Aragon"
+    def __init__(self, nom, ed, carr):
+        self.nombre = nom
+        self.edad = ed
+        self.carrera = carr
+
---
+++ mainpoo.py
@@ -0,0 +1,29 @@
+from cosas import Alumno
+
+def main():
+
+    """
+    al1 = Alumno()
+    print(al1)
+    al2= Alumno()
+    print(al1.facultad)
+    print(al2.facultad)
+    print(Alumno.facultad)
+    print("-----------")
+    al1.facultad = "FES Aragon EDOMEX"
+    print(al1.facultad)
+    print(al2.facultad)
+    print(Alumno.facultad)
+    print("-----Un vistazo a los objetos")
+    print(vars(al1))
+    print(vars(al2))
+    print("----modificar atributos publicos")
+    al1.edad= 999
+    print(vars(al1))
+    print(vars(al2))
+    """
+
+   al1 = Almuno("Diego", 19, "ICO")
+   al2 = Alumno("Monse", 20, "Derecho")
+
+main()
